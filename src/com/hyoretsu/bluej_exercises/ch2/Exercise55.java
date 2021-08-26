47f417f4ad117eb5899b13cdc5e1a7463ef9532c
Aran Leite <hyoretsu@gmail.com>
2021-08-26 00:59:16 -0300

    Exercise 2.55

---
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise55.java
@@ -0,0 +1,9 @@
+package com.hyoretsu.bluej_exercises.ch2;
+
+public class Exercise55 {
+ public Integer mean;
+
+ public Exercise55(Integer total, Integer count) {
+  this.mean = total / count;
+ };
+}
