703da2ec151113144c86926e8b71260d83ac0ec3
Aran Leite <hyoretsu@gmail.com>
2021-08-26 00:48:06 -0300

    Exercise 2.33

---
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise33.java
@@ -0,0 +1,10 @@
+package com.hyoretsu.bluej_exercises.ch2;
+
+public class Exercise33 {
+ public Integer score = 0;
+
+ /** Increase score by the given number of points. */
+ public void increase(int points) {
+  this.score += points;
+ }
+}
