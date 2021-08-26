42207c15b29f33a67ee54a1498c0e67bee583e94
Aran Leite <hyoretsu@gmail.com>
2021-08-26 01:02:33 -0300

    Exercise 2.56

---
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise56.java
@@ -0,0 +1,11 @@
+package com.hyoretsu.bluej_exercises.ch2;
+
+public class Exercise56 {
+ public Exercise56(Integer price, Integer budget) {
+  if (price > budget) {
+   System.out.println("Too expensive");
+  } else {
+   System.out.println("Just right");
+  }
+ }
+}
