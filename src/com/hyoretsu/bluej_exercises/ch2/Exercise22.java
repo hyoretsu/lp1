5480f8b18fe220b574e78bbeea00cd29ae153ede
Aran Leite <hyoretsu@gmail.com>
2021-08-26 00:41:39 -0300

    Exercise 2.22

---
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise22.java
@@ -0,0 +1,13 @@
+package com.hyoretsu.bluej_exercises.ch2;
+
+public class Exercise22 {
+ public Integer day;
+ public String month;
+ public Integer year;
+
+ public Exercise22(String month, Integer day, Integer year) {
+  this.day = day;
+  this.month = month;
+  this.year = year;
+ }
+}
