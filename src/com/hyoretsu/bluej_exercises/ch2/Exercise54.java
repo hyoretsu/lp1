06e2c1fa805541fe22cf127a17bd56877f8db70c
Aran Leite <hyoretsu@gmail.com>
2021-08-26 00:51:56 -0300

    Exercise 2.54

---
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise54.java
@@ -0,0 +1,9 @@
+package com.hyoretsu.bluej_exercises.ch2;
+
+public class Exercise54 {
+ public Integer saving;
+
+ public Exercise54(Integer price, Float discount) {
+  this.saving = (int) (price * discount);
+ };
+}
