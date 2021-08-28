f36da8d6bdb9c8132102228bdb8c0536af0b4903
Aran Leite <hyoretsu@gmail.com>
2021-08-28 01:29:01 -0300

    Exercise 2.70

--- src/com/hyoretsu/bluej_exercises/ch2/Exercise70.java
+++ src/com/hyoretsu/bluej_exercises/ch2/Exercise70.java
@@ -1,13 +1,17 @@
 package com.hyoretsu.bluej_exercises.ch2;
 
-public class Exercise69 {
+public class Exercise70 {
  public String name;
  public Integer age;
  public String code;
  public Integer credits;
 
- public Exercise69(String myName, Integer myAge) {
+ public Exercise70(String myName, Integer myAge) {
   this.name = myName;
   this.age = myAge;
+ }
+
+ public Integer getAge() {
+  return this.age;
  }
 }
