a560884ad7762154169bbae27374915d40e3aa90
Aran Leite <hyoretsu@gmail.com>
2021-08-29 20:36:30 -0300

    Exercise 2.94

--- src/com/hyoretsu/lp1/Heater.java
+++ src/com/hyoretsu/lp1/Heater.java
@@ -1,21 +1,45 @@
 package com.hyoretsu.lp1;
 
 public class Heater {
+ private Double increment;
+ private Double max;
+ private Double min;
  public Double temperature;
 
- public Heater() {
+ public Heater(Double min, Double max) {
+  this.increment = 5.0;
+  this.max = max;
+  this.min = min;
   this.temperature = 15.0;
  }
 
  public void cooler() {
-  this.temperature -= 5;
+  Double newTemp = this.temperature - this.increment;
+
+  if (newTemp >= this.min) {
+   this.temperature = newTemp;
+  } else {
+   System.out.println("This action would make the heater colder than its minimum temperature");
+  }
  }
 
  public Double getTemperature() {
   return this.temperature;
  }
 
+ public void setIncrement(Double increment) {
+  if (increment > 0) {
+   this.increment = increment;
+  }
+ }
+
  public void warmer() {
-  this.temperature += 5;
+  Double newTemp = this.temperature + this.increment;
+
+  if (newTemp <= this.max) {
+   this.temperature = newTemp;
+  } else {
+   System.out.println("This action would make the heater hotter than its maximum temperature");
+  }
  }
 }
