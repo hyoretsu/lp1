9dde7747d3fabd4fdf575646e00496627d41e74b
Aran Leite <hyoretsu@gmail.com>
2021-08-31 22:40:51 -0300

    Exercise 3.40

---
+++ src/main/java/com/hyoretsu/lp1/house/Tree.java
@@ -0,0 +1,12 @@
+package com.hyoretsu.lp1.house;
+
+// Exercise 3.40
+public class Tree {
+ private Triangle leaves;
+ private Square trunk;
+
+ public Tree() {
+  this.leaves = new Triangle();
+  this.trunk = new Square();
+ }
+}
