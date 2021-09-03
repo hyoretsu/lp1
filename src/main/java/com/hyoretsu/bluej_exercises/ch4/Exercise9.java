7e89796f19671846642f7606ab95f56a8ab7ea23
Aran Leite <hyoretsu@gmail.com>
2021-09-03 15:34:41 -0300

    Exercise 4.9

---
+++ src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise9.java
@@ -0,0 +1,11 @@
+package com.hyoretsu.bluej_exercises.ch4;
+
+import java.util.ArrayList;
+
+public class Exercise9 {
+ private ArrayList<Integer> items;
+
+ public Exercise9() {
+  items.get(4);
+ }
+}
