bbfa579977da6a3817d220e11addaf980e0b81a9
Aran Leite <hyoretsu@gmail.com>
2021-09-03 15:08:29 -0300

    Exercise 4.5

--- src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise5.java
+++ src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise5.java
@@ -1,8 +1,10 @@
 package com.hyoretsu.bluej_exercises.ch4;
 
 import com.hyoretsu.lp1.Book;
+import com.hyoretsu.lp1.lab_classes.Student;
 import java.util.ArrayList;
 
 public class Exercise4 {
+ private ArrayList<Student> cs101;
  private ArrayList<Book> library;
 }
