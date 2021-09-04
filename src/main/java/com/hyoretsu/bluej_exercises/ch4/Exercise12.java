945c4a6afca4fa5fb76ddfd9e50975f35dc6c550
Aran Leite <hyoretsu@gmail.com>
2021-09-04 11:24:19 -0300

    Exercise 4.12

---
+++ src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise12.java
@@ -0,0 +1,11 @@
+package com.hyoretsu.bluej_exercises.ch4;
+
+import java.util.List;
+
+public class Exercise12 {
+ private List<Object> dates;
+
+ public Exercise12() {
+  this.dates.remove(2);
+ }
+}
