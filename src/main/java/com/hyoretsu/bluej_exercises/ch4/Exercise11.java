2da667343abf9dbebb03f8d3ab6eaf4cca58f602
Aran Leite <hyoretsu@gmail.com>
2021-09-04 11:12:35 -0300

    Exercise 4.11

---
+++ src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise11.java
@@ -0,0 +1,12 @@
+package com.hyoretsu.bluej_exercises.ch4;
+
+import java.util.List;
+
+public class Exercise11 {
+ private List<Object> files;
+ private Object favoriteTrack;
+
+ public Exercise11() {
+  this.files.add(this.favoriteTrack);
+ }
+}
