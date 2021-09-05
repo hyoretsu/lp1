46662d832f8e7a96c9dec7c9f427b4cd74069bca
Aran Leite <hyoretsu@gmail.com>
2021-09-05 02:28:24 -0300

    Exercise 4.14

---
+++ src/main/java/com/hyoretsu/lp1/music_organizer/MusicOrganizer.java
@@ -0,0 +1,63 @@
+package com.hyoretsu.lp1.music_organizer;
+
+import java.util.ArrayList;
+
+public class MusicOrganizer {
+ /** An ArrayList for storing the file names of music files. */
+ private ArrayList<String> files;
+
+ /** Create a MusicOrganizer */
+ public MusicOrganizer() {
+  files = new ArrayList<>();
+ }
+
+ /**
+  * Add a file to the collection.
+  *
+  * @param filename The file to be added.
+  */
+ public void addFile(String filename) {
+  files.add(filename);
+ }
+
+ // Exercise 4.14
+ public void checkIndex(Integer index) {
+  Integer size = files.size();
+
+  if (index < 0 || index >= size) {
+   System.out.println("The valid range for currently stored musics is: 0" + ((size == 1) ? "" : ("~" + (size - 1))));
+  }
+ }
+
+ /**
+  * Return the number of files in the collection.
+  *
+  * @return The number of files in the collection.
+  */
+ public int getNumberOfFiles() {
+  return files.size();
+ }
+
+ /**
+  * List a file from the collection.
+  *
+  * @param index The index of the file to be listed.
+  */
+ public void listFile(int index) {
+  if (index >= 0 && index < files.size()) {
+   String filename = files.get(index);
+   System.out.println(filename);
+  }
+ }
+
+ /**
+  * Remove a file from the collection.
+  *
+  * @param index The index of the file to be removed.
+  */
+ public void removeFile(int index) {
+  if (index >= 0 && index < files.size()) {
+   files.remove(index);
+  }
+ }
+}
