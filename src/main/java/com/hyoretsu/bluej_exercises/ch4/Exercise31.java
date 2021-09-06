7da5646bd057f66002de0b32bc82c536aa3a6a8c
Aran Leite <hyoretsu@gmail.com>
2021-09-05 22:14:28 -0300

    Exercise 4.31

---
+++ src/main/java/com/hyoretsu/bluej_exercises/ch4/Exercise31.java
@@ -0,0 +1,14 @@
+package com.hyoretsu.bluej_exercises.ch4;
+
+public class Exercise31 {
+ public Exercise31() {
+  Integer total = 0;
+
+  Integer i = 1;
+  while (i <= 10) {
+   total += i;
+  }
+
+  System.out.println("The sum of all values between 1 and 10 is " + total);
+ }
+}
