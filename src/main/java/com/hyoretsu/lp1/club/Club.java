0fa26d4681b48a3f41139202495465797ec0165e
Aran Leite <hyoretsu@gmail.com>
2021-09-06 16:28:52 -0300

    Exercise 4.40

---
+++ src/main/java/com/hyoretsu/lp1/club/Club.java
@@ -0,0 +1,26 @@
+package com.hyoretsu.lp1.club;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Club {
+ // Exercise 4.40
+ private List<Membership> memberships = new ArrayList<>();
+
+ /** Constructor for objects of class Club */
+ public Club() {
+ }
+
+ /**
+  * Add a new member to the club's list of members.
+  *
+  * @param member The member object to be added.
+  */
+ public void join(Membership member) {
+ }
+
+ /** @return The number of members (Membership objects) in the club. */
+ public int numberOfMembers() {
+  return 0;
+ }
+}
