2eb89654ac99b8ea4ce5d407d510bc4812c0f805
Aran Leite <hyoretsu@gmail.com>
2021-09-18 17:35:47 -0300

    Exercise 9.13

---
+++ src/main/java/com/hyoretsu/lp1/online_shop/CommentTest.java
@@ -0,0 +1,31 @@
+package com.hyoretsu.lp1.online_shop;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+// Exercise 9.13
+public class CommentTest {
+ /** Default constructor for test class CommentTest */
+ public CommentTest() {
+ }
+
+ /**
+  * Sets up the test fixture.
+  *
+  * Called before every test case method.
+  */
+ @Before
+ public void setUp() {
+ }
+
+ /**
+  * Tears down the test fixture.
+  *
+  * Called after every test case method.
+  */
+ @After
+ public void tearDown() {
+ }
+}
